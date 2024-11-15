8b710d09f442918aba177eb18fde9b6b1d5c6e83
TomasRacciatti <tomasracciatti@gmail.com>
2024-11-15 19:57:49 -0300

    intento de resolucion de pantalla de victoria

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -7,14 +7,41 @@ public class Player : Agent
 {
     [SerializeField] protected float _speed;
     [SerializeField] private LayerMask _victoryLayerMask;
+    [SerializeField] private Vector3 _collisionBoxSize = new Vector3(1, 2, 1);
 
-    private void OnTriggerEnter(Collider other)
+
+    private void Update()
     {
-        if (other.gameObject.layer == _victoryLayerMask)
+        Vector3 overlapCenter = transform.position;
+        Collider[] hits = Physics.OverlapBox(overlapCenter, _collisionBoxSize / 2, Quaternion.identity, _victoryLayerMask);
+
+        foreach (var hit in hits)
         {
-            Debug.Log("ganaste");
+            Debug.Log($"Player detected trigger zone: {hit.gameObject.name}");
+
+            if (((1 << hit.gameObject.layer) & _victoryLayerMask) != 0)
+            {
+                Debug.Log("Victory");
+                GameOverManager.Instance.gameWon = true;
+                GameOverManager.Instance.TriggerVictory();
+                return;
+            }
+        }
+
+        /*
+        if (hits.Length > 0)
+        {
+
             GameOverManager.Instance.gameWon = true;
             GameOverManager.Instance.TriggerVictory();
         }
+        */
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Vector3 boxCenter = transform.position;
+        Gizmos.DrawWireCube(boxCenter, _collisionBoxSize);
     }
 }
